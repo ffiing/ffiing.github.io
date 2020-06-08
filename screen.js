6e53ed74d6267a0f4820cae75057640a9ee53392
ffiing <ffiing@qq.com>
2020-06-08 14:18:21 +0800

    Update screen.js

--- screen.js
+++ screen.js
@@ -1,2 +1,18 @@
 // 
-console.log('脚本生效了')
+console.log('脚本生效了234243')
+
+ var height1 = window.screen.height;
+                var availHeight = screen.availHeight;
+                var width1 = screen.width;
+                var availWidth = screen.availWidth;
+                
+                var str1 = 'height1=' + height1 + 'width1=' + width1 + 'availHeight=' + availHeight + 'availWidth=' + availWidth;
+
+                var innerHeight = window.innerHeight;
+                var outerHeight = window.outerHeight;
+                var innerWidth = window.innerWidth;
+                var outerWidth = window.outerWidth;
+                
+                var str2 = 'innerHeight='+innerHeight + 'innerWidth='+innerWidth+'outerHeight='+outerHeight+'outerWidth='+outerWidth;
+                alert(str1)
+                alert(str2)
